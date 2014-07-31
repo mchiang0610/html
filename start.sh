f58f8bea1211c9f103a30b9008945179645cac51
Sean Li <lishang106@gmail.com>
2014-07-31 15:14:55 -0700

    Default to kite.

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-adduser --disabled-password --gecos "" $SSH_USER; usermod -a -G sudo $SSH_USER
-echo "$SSH_USER	ALL=NOPASSWD: ALL" >> /etc/sudoers
-mkdir /home/$SSH_USER/.ssh
-mv /app /home/$SSH_USER
-mv /.bashrc /home/$SSH_USER
-chown -R $SSH_USER /home/$SSH_USER
-chgrp -R $SSH_USER /home/$SSH_USER
+adduser --disabled-password --gecos "" kite; usermod -a -G sudo kite
+echo "kite	ALL=NOPASSWD: ALL" >> /etc/sudoers
+mkdir /home/kite/.ssh
+mv /app /home/kite
+mv /.bashrc /home/kite
+chown -R kite /home/kite
+chgrp -R kite /home/kite
 chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
-echo $SSH_KEY > /home/$SSH_USER/.ssh/authorized_keys
+echo $SSH_KEY > /home/kite/.ssh/authorized_keys
 /usr/local/bin/supervisord -n
