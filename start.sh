5f4821790493e900f32d5bba50d391176500fb47
Sean Li <lishang106@gmail.com>
2014-07-31 11:28:41 -0700

    Order change.

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 adduser --disabled-password --gecos "" $SSH_USER; usermod -a -G sudo $SSH_USER
 echo "$SSH_USER	ALL=NOPASSWD: ALL" >> /etc/sudoers
 mkdir /home/$SSH_USER/.ssh
+mv /app /home/$SSH_USER
+mv /.bashrc /home/$SSH_USER
 chown -R $SSH_USER /home/$SSH_USER
 chgrp -R $SSH_USER /home/$SSH_USER
 chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
-mv /.bashrc /home/$SSH_USER
 echo $SSH_KEY > /home/$SSH_USER/.ssh/authorized_keys
-mv /app /home/$SSH_USER
 /usr/local/bin/supervisord -n
