057b7e0dd76359a0b09fe21f518e108451752c25
Sean Li <lishang106@gmail.com>
2014-08-15 19:42:16 -0700

    Changed name.

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-adduser --disabled-password --gecos "" kite; usermod -a -G sudo kite
-echo "kite	ALL=NOPASSWD: ALL" >> /etc/sudoers
-mkdir /home/kite/.ssh
-mv /.bashrc /home/kite
-chown -R kite /home/kite
-chgrp -R kite /home/kite
+adduser --disabled-password --gecos "" user; usermod -a -G sudo user
+echo "user	ALL=NOPASSWD: ALL" >> /etc/sudoers
+mkdir /home/user/.ssh
+mv /.bashrc /home/user
+chown -R user /home/user
+chgrp -R user /home/user
 chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
-echo $SSH_KEY > /home/kite/.ssh/authorized_keys
+echo $SSH_KEY > /home/user/.ssh/authorized_keys
 /usr/sbin/nginx &
 /usr/sbin/sshd -D
